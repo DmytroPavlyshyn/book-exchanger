6a0dd14c6477888c2f64b28b04d3445855376b40
Dmytro <dimapavlyshyn@gmail.com>
2019-03-28 21:09:04 +0200

    Initial project structure

---
+++ book-exchanger-api/src/main/java/bookexchange/api/BookExchangerApiApplication.java
@@ -0,0 +1,13 @@
+package bookexchange.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookExchangerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookExchangerApiApplication.class, args);
+	}
+
+}
